20c61411f4c7e6982a833b8ababe19c465f33cfe
Lampros <hauahx@gmail.com>
2022-05-16 00:25:44 +0300

    back to main after breaking..

--- lua/attempt/config.lua
+++ lua/attempt/config.lua
@@ -2,37 +2,14 @@ local M = {}
 
 local unix = vim.fn.has 'unix' == 1
 local path_separator = unix and '/' or '\\'
+local cpp_compiler = "g++"
+local c_compiler = "gcc"
 
 local function initial_content_fn(ext)
-	return require('attempt.initial_content.' .. ext)
+  return require('attempt.initial_content.' .. ext)
 end
 
 local defaults = {
-<<<<<<< HEAD
-	dir = (unix and '/tmp/' or vim.fn.expand '$TEMP\\') .. 'attempt.nvim' .. path_separator,
-	autosave = false,
-	list_buffers = false,
-	initial_content = {
-		py = initial_content_fn,
-		c = initial_content_fn,
-		cpp = initial_content_fn,
-		java = initial_content_fn,
-		rs = initial_content_fn,
-		go = initial_content_fn,
-		sh = initial_content_fn
-	},
-	ext_options = { 'lua', 'js', 'py', 'cpp', 'c', '' },
-	format_opts = { [''] = '[None]' },
-	run = {
-		py = { 'w !python' },
-		js = { 'w !node' },
-		lua = { 'w' , 'luafile %' },
-		sh = { 'w !bash' },
-		pl = { 'w !perl' },
-		cpp = { 'w' , '!' .. cpp_compiler .. ' % -o %:p:r.out && %:p:r.out' },
-		c = { 'w' , '!' .. c_compiler .. ' % -o %:p:r.out && %:p:r.out' },
-	}
-||||||| f846e9f (added C/C++ run cmds)
   dir = (unix and '/tmp/' or vim.fn.expand '$TEMP\\') .. 'attempt.nvim' .. path_separator,
   autosave = false,
   list_buffers = false,
@@ -56,36 +33,15 @@ local defaults = {
     cpp= 'w | !'.. cpp_compiler ..' % -o %:p:r.out && %:p:r.out',
     c= 'w | !'.. c_compiler ..' % -o %:p:r.out && %:p:r.out',
   }
-=======
-  dir = (unix and '/tmp/' or vim.fn.expand '$TEMP\\') .. 'attempt.nvim' .. path_separator,
-  autosave = false,
-  list_buffers = false,
-  initial_content = {
-    py = initial_content_fn,
-    c = initial_content_fn,
-    cpp = initial_content_fn,
-    java = initial_content_fn,
-    rs = initial_content_fn,
-    go = initial_content_fn,
-    sh = initial_content_fn
-  },
-  ext_options = { 'lua', 'js', 'py', 'cpp', 'c', '' },
-  format_opts = { [''] = '[None]' },
-  run = {
-    py = 'w !python',
-    js = 'w !node',
-    lua = 'w | luafile %',
-    sh = 'w !bash',
-    pl = 'w !perl'
-  }
->>>>>>> parent of f846e9f (added C/C++ run cmds)
 }
 
 function M.setup(opts)
-	M.opts = vim.tbl_deep_extend("force", {}, defaults, opts or {})
-	if not string.find(M.opts.dir, path_separator .. '$') then
-		M.opts.dir = M.opts.dir .. path_separator
-	end
+  M.opts = vim.tbl_deep_extend("force", {}, defaults, opts or {})
+  if not string.find(M.opts.dir, path_separator .. '$') then
+    M.opts.dir = M.opts.dir .. path_separator
+  end
 end
 
 return M
+
+
