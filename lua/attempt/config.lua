070fa6c774d734aa697b16a695c49ad6977bdbdf
Lampros <hauahx@gmail.com>
2022-05-16 12:42:09 +0300

    Added C/CPP run cmds

--- lua/attempt/config.lua
+++ lua/attempt/config.lua
@@ -3,6 +3,9 @@ local M = {}
 local unix = vim.fn.has 'unix' == 1
 local path_separator = unix and '/' or '\\'
 
+local cpp_compiler = "g++"
+local c_compiler = "gcc"
+
 local function initial_content_fn(ext)
 	return require('attempt.initial_content.' .. ext)
 end
@@ -28,6 +31,8 @@ local defaults = {
 		lua = { 'w', 'luafile %' },
 		sh = { 'w !bash' },
 		pl = { 'w !perl' },
+		cpp = { "w" , '!'.. cpp_compiler ..'% -o %:p:r.out && echo "" && %:p:r.out'},
+		c = { "w" , '!'.. c_compiler ..'% -o %:p:r.out && echo "" && %:p:r.out'},
 	}
 }
 
